c578fa9d9fb8c66aa44b0fc2ff1a8863cb689483
Kou Nebelfee <ygeming1225@gmail.com>
2025-03-12 20:28:26 +0900

    新しいAPIキーに変更

--- api/index/index.php
+++ api/index/index.php
@@ -3,7 +3,7 @@ include __DIR__ . "/../includes/header.php";
 $url = "https://Kou-Nebelfee-s-workspace-j308qh.us-east-1.xata.sh/db/ff14_db:main/tables/uploads";
 $context = stream_context_create([
     "http" => [
-        "header" => "Authorization: Bearer xau_bfTIFSguQmxuCs0Pm7wRh5ZA1kwtD7PP3"
+        "header" => "Authorization: Bearer xau_HlG7vQuTgRvwFLB5DRtct5hQEu6LZBVBe"
     ]
 ]);
 $response = file_get_contents($url, false, $context);
